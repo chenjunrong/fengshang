fb09962fafe2f32e368acf1ad45768fb7995e7e7
cjr <290383910@qq.com>
2018-03-24 11:36:30 +0800

    修改精隆

--- html/201803/jinglong/js/product.js
+++ html/201803/jinglong/js/product.js
@@ -1,15 +1,49 @@
-$(window).load(function(){
-	$(".type-tabbar-swiper").on("click",".swiper-slide",function(){
-        	$(".type-tabbar-swiper .swiper-slide").removeClass("active");
-        	$(this).addClass("active");
-        	var index=$(this).index();
-        	type_tabbar_swiper.slideTo(index, 300, false);
-        });
-});
+		$(window).load(function(){    
+			
+            $(".inside-type-swiper").on("click",".swiper-slide",function(){
+	        	$(".inside-type-swiper .swiper-slide").removeClass("active");
+	        	$(this).addClass("active");
+	        });
+
+            /*遍历*/
+            if($(".inside-type-swiper").length>0){
+		        $.each($(".inside-type-swiper .swiper-slide"),function (index,domEle){
+		            var typeW=$(this).find("a").width();
+		            $(this).css("width",typeW);         
+		        });
+		        /*更新swiper-slide宽度*/
+		        inside_type_swiper.update();
+	        }
 
 
+            $(".type-tabbar-swiper").on("click",".swiper-slide",function(){
+	        	$(".type-tabbar-swiper .swiper-slide").removeClass("active");
+	        	$(this).addClass("active");
+	        });
+
+            /*遍历*/
+            if($(".type-tabbar-swiper").length>0){
+		        $.each($(".type-tabbar-swiper .swiper-slide"),function (index,domEle){
+		            var typeW=$(this).find("a").width();
+		            $(this).css("width",typeW);         
+		        });
+		        /*更新swiper-slide宽度*/
+		        type_tabbar_swiper.update();
+	        }
+	                   
+		});
+		
+		
+		var inside_type_swiper = new Swiper('.inside-type-swiper', {
+            slidesPerView :'auto'
+		});
+		var type_tabbar_swiper = new Swiper('.type-tabbar-swiper', {
+            slidesPerView :'auto'
+		});	
+		
+			
 
 
-var type_tabbar_swiper = new Swiper('.type-tabbar-swiper', {
-    slidesPerView :6.5
-});	
+		
+	
+		
